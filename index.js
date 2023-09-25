65a535634b424ea2052d88f0128af8df962bc9f9
Pranay <pny1cl2125@gmail.com>
2023-09-25 22:20:51 +0530

    changes

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
 
   app.on("issues.opened", async (context) => {
-    console.log(context);
+    console.log(context.payload);
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
     return context.octokit.issues.createComment(issueComment);
   });
   app.on("issues.closed", async (context)=>{
-    console.log(context);
+    console.log(context.payload);
   });
-  app.on("pull_request",async (context)=>{console.log(context);});
+  app.on("pull_request",async (context)=>{console.log(context.payload);});
 
   // For more information on building apps:
   // https://probot.github.io/docs/
