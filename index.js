06d22fe9cb23611ca1f0d933f38954760731f31d
Pranay <pny1cl2125@gmail.com>
2023-09-26 16:07:09 +0530

    add

--- index.js
+++ index.js
@@ -5,16 +5,25 @@
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
-
   app.on("issues.opened", async (context) => {
-    console.log(context.payload);
+    console.log("----------------------------Issue Opened--------------------------");
+    console.log("action: "+context.payload.action);
+    console.log("repo name: "+context.payload.repository.name);
+    console.log("issue title: "+context.payload.issue.title);
+    console.log("issue body: "+context.payload.issue.body);
+    console.log("-------------------------------------------------------------------");
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
     return context.octokit.issues.createComment(issueComment);
   });
   app.on("issues.closed", async (context)=>{
-    console.log(context.payload);
+    console.log("-----------------------------Issue Closed-------------------------");
+    console.log("action: "+context.payload.action);
+    console.log("repo name: "+context.payload.repository.name);
+    console.log("issue title: "+context.payload.issue.title);
+    console.log("issue body: "+context.payload.issue.body);
+    console.log("-------------------------------------------------------------------");
   });
   app.on("pull_request",async (context)=>{console.log(context.payload);});
 
